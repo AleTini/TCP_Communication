f1ae43c8ed0856c67277a5bf0c0acbca0480b5fe
AleTini <pizzy.tini.007@gmail.com>
2026-02-17 18:11:54 +0100

    modifica classe con aggiunta invio richiesta e lettura risposta server con PrintWriter e BufferedReader

--- src/MainClient/MainClient.java
+++ src/MainClient/MainClient.java
@@ -1,15 +1,27 @@
 package MainClient;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class MainClient {
     public static void main(String[] args) {
         System.out.println("CLIENT: avvio del client!");
         try {
-            Socket socket = new Socket("localhost",3000);
+            // Richiesta connessione
+            Socket socket = new Socket("localhost", 3000);
+
+            // Invio richiesta
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            writer.println("CLIENT: ciao!");
+
+            // Lettura risposta
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String risposta = reader.readLine();
+            System.out.println("CLIENT: ricevuto dal server: " + risposta);
+
+            // Chiusura comunicazione
+            socket.close();
+
         } catch (IOException e) {
             System.out.println("Errore nella connessione con il server");
         }
--- src/MainServer/MainServer.java
+++ src/MainServer/MainServer.java
@@ -1,6 +1,6 @@
 package MainServer;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,18 +8,30 @@ public class MainServer {
     public static void main(String[] args) {
         System.out.println("SERVER: inizio comunicazione");
         try {
+            // Avvio server
             ServerSocket server = new ServerSocket(3000);
             System.out.println("SERVER: pronto in attesa di richieste dei client");
+
+            // Connessione con il client
             Socket clientSocket = server.accept();
             System.out.println("SERVER: connesso al client");
-            // comunicazione
-            clientSocket.close(); // chiusura data socket
-            server.close(); // chiusura connection socket
 
+            // Lettura richiesta del client
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String richiesta = reader.readLine();
+            System.out.println("SERVER: ricevuto dal client: " + richiesta);
+
+            // Invio risposta al client
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+            writer.println("SERVER: messaggio ricevuto!");
+
+            // Chiusura comunicazione
+            clientSocket.close();
+            // Chiusura server
+            server.close();
 
         } catch (IOException e) {
-            System.err.println("Porta non disponibile, errore nella " +
-                    "creazione del socket");
+            System.err.println("Porta non disponibile, errore nella creazione del socket");
         }
     }
 }
